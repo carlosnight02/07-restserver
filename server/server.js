437004ce42d88d2d66477cd6bda24fa05c5b9147
carlosnight02 <ca.murillo1@hotmail.com>
2020-01-27 16:56:43 -0500

    Creando un usuario en la coleccion

--- server/server.js
+++ server/server.js
@@ -2,48 +2,21 @@ require('./config/config');
 
 const express = require('express');
 const app = express();
+const mongoose= require('mongoose');
 
 const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost:27017/cafe', {useNewUrlParser: true}, (err, res) => {
+    if(err) throw err;
+    console.log("BD online")
+});
 
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json())
 
- 
-app.get('/usuario', function (req, res) {
-  res.json('getUsuario')
-});
- 
-app.post('/usuario', function (req, res) {
-
-    let body= req.body
-
-    if(body.nombre=== undefined){
-        res.status(400).json({
-            ok: false,
-            message:'El nombre es necesario'
-        });
-    }else{
-        res.json({
-            persona:body
-        });
-    }
-});
-
-app.put('/usuario/:id', function (req, res) {
-
-    let id= req.params.id;
-    res.json({
-        id
-    });
-
-});
-
-app.put('/usuario', function (req, res) {
-    res.json('deletUsuario')
-});
-
+app.use(require('./routes/usuario'));
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Escuchando puerto ${process.env.PORT}`)
